28a7218c773f7f5dceb15fb7c7f8d1fd3a14d0f7
shivajikhot <shivajkv@gmail.com>
2025-01-15 14:45:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "ec2_instance_id" {
   value = aws_instance.example_instance.id
 }
  
-output "ec2_instance_public_ip" {
+output "instance_public_ip" {
   value = aws_instance.example_instance.public_ip
 }
